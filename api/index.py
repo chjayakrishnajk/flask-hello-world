c63a742fbf6d780534b8e8c381bbf681034552cc
Itachi <69712032+chjayakrishnajk@users.noreply.github.com>
2024-04-16 15:02:29 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,10 +2,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+status = "Ntg"
+
 @app.route('/')
 def home():
-    return 'Hello, World!'
-
+    if(status == "Ntg"):
+        status == "Yes"
+    return status
+@app.route('/no')
+def home():
+    status == "No"
+    return status
+@app.route('/status')
+def home():
+    return status
 @app.route('/about')
 def about():
     return 'About'
